91972dce7c028a4cb0cd29659f3660c79fd6258d
Aditya <singhaditya5711@gmail.com>
2025-08-22 23:01:31 +0530

    added video circle reveal animation

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ import { useGSAP } from "@gsap/react";
 import MessageSection from "./sections/MessageSection";
 import FlavorSection from "./sections/FlavorSection";
 import NutritionSection from "./sections/NutritionSection";
+import BenefitSection from "./sections/BenefitSection";
 
 gsap.registerPlugin(ScrollTrigger, ScrollSmoother);
 
@@ -27,6 +28,7 @@ const App = () => {
           <MessageSection />
           <FlavorSection />
           <NutritionSection />
+          <BenefitSection />
         </div>
       </div>
     </main>
